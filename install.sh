13d606b8fc3d0f1b3423732052e7b0213bfd3a20
obrien-james <jameswobrien1987@gmail.com>
2020-05-17 08:05:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
+#! /bin/bash
+#####################################################
+## Created by James O'Brien                        ##
+## Date Created 17/05/2020                         ##
+## Script to run Hikvision streams on Raspberry Pi ##
+##  - Install file - use: on first install only    ##
+#####################################################
 
+echo "Installing Hik-Viewer ..."
+
+#Make new directory:
+mkdir ~/hik/
+
+#Move files from to new directory:
+cp ./ ~/hik/
+
+#Remove uncecessary files
+rm ~/hik/install.sh
+rm ~/hik/README.md
+
+#Edit file permissions to allow scripts to run:
+chmod 744 ~/hik/*.sh
+
+#End of install.
+echo "Installation complete"
+echo "Please edit ~/hik/cameras.config with necessary files"
+echo "Once cameras.config has been configured - run ~/hik/hik.sh start"
