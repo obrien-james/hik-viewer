860effdfd3963b03c305de55f1ff13d13258c2a9
obrien-james <jameswobrien1987@gmail.com>
2020-07-11 16:57:35 +0100

    Update hik-server.py

--- hik-server.py
+++ hik-server.py
@@ -18,49 +18,56 @@ PORT = 8080
 
 
 class MainHandler(tornado.web.RequestHandler):
-  def get(self):
-     print ("[HTTP](MainHandler) User Connected.")
-     self.render("index.html")
+	def get(self):
+		print ("[HTTP](MainHandler) User Connected.")
+		self.render("index.html")
 
 	
 class WSHandler(tornado.websocket.WebSocketHandler):
-  def open(self):
-    print ('[WS] Connection was opened.')
- 
-  def on_message(self, message):
-    print ('[WS] Incoming message:'), message
-    
-    if message == "Preset1":
-      print ('Changing to preset 1')
-      subprocess.run(["~/hik/changepreset.sh 1"], shell=True)
-    if message == "Preset2":
-      print ('Changing to preset 2')
-      subprocess.run(["~/hik/changepreset.sh 2"], shell=True)      
-    if message == "Preset3":
-      print ('Changing to preset 3')
-      subprocess.run(["~/hik/changepreset.sh 3"], shell=True)
-    if message == "Preset4":
-      print ('Changing to preset 4')
-      subprocess.run(["~/hik/changepreset.sh 4"], shell=True)
-    if message == "Preset5":
-      print ('Changing to preset 5')
-      subprocess.run(["~/hik/changepreset.sh 5"], shell=True)
-    if message == "Preset6":
-      print ('Changing to preset 6')
-      subprocess.run(["~/hik/changepreset.sh 6"], shell=True)      
-    if message == "Preset7":
-      print ('Changing to preset 7')
-      subprocess.run(["~/hik/changepreset.sh 7"], shell=True)
-    if message == "Preset8":
-      print ('Changing to preset 8')
-      subprocess.run(["~/hik/changepreset.sh 8"], shell=True)
+	def open(self):
+		print ('[WS] Connection was opened.')
+		
+	def on_message(self, message):
+		print ('[WS] Incoming message:'), message
+		
+		if message == "Preset1":
+			print ('Changing to preset 1')
+			subprocess.run(["~/hik/changepreset.sh 1"], shell=True)
+			
+		if message == "Preset2":
+			print ('Changing to preset 2')
+			subprocess.run(["~/hik/changepreset.sh 2"], shell=True)
+		
+		if message == "Preset3":
+			print ('Changing to preset 3')
+			subprocess.run(["~/hik/changepreset.sh 3"], shell=True)
+			
+		if message == "Preset4":
+			print ('Changing to preset 4')
+			subprocess.run(["~/hik/changepreset.sh 4"], shell=True)
+			
+		if message == "Preset5":
+			print ('Changing to preset 5')
+			subprocess.run(["~/hik/changepreset.sh 5"], shell=True)
+		
+		if message == "Preset6":
+			print ('Changing to preset 6')
+			subprocess.run(["~/hik/changepreset.sh 6"], shell=True)
+			
+		if message == "Preset7":
+			print ('Changing to preset 7')
+			subprocess.run(["~/hik/changepreset.sh 7"], shell=True)
+			
+		if message == "Preset8":
+			print ('Changing to preset 8')
+			subprocess.run(["~/hik/changepreset.sh 8"], shell=True)
+			
+		if message == 'reload':
+			print ('Reloading screens ...')
+			subprocess.run(["~/hik/hik.sh reload"], shell=True)
 
-    if message == 'reload':
-      print ('Reloading screens ...')
-      subprocess.run(["~/hik/hik.sh reload"], shell=True)
-    
-  def on_close(self):
-    print ('[WS] Connection was closed.')
+	def on_close(self):
+		print ('[WS] Connection was closed.')
 
 
 application = tornado.web.Application([
