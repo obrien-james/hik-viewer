a204b938414fd08df22675b845f18d700075f2c2
obrien-james <jameswobrien1987@gmail.com>
2020-07-11 12:21:30 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,1 +1,9 @@
+#! /bin/bash
+########################################################
+## Created by James O'Brien                           ##
+## Date Created 11/07/2020                            ##
+## Script to upgrade hik-viewer software              ##
+##  - Upgrade file - use: Runs after repo update      ##
+########################################################
 
+echo "Upgrading Hik-Viewer ..."
