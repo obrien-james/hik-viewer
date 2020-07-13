85230a6d4f3f9fc71dc9af31237c5f3432310c68
obrien-james <jameswobrien1987@gmail.com>
2020-07-13 16:24:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,26 +9,33 @@
 echo "Installing Hik-Viewer ..."
 
 #Make new directory:
+echo "Making new directory"
 mkdir ~/hik/
 
 #Move files from to new directory:
-cp ./ ~/hik/
+echo "Copying files"
+cp -r ./ ~/hik/
+
+#Edit file permissions to allow scripts to run:
+echo "Updating permissions ... "
+chmod 744 ~/hik/*.sh
+chmod 744 ~/hik/*.py
 
 #Remove uncecessary files
+echo "Remove unnecessary files"
 rm ~/hik/install.sh
 rm ~/hik/upgrade.sh
 rm ~/hik/README.md
 
 #Install tornado - to run websockets
+echo "Install tornado"
 sudo pip3 install tornado
 
 #update crontab to check HikViewer running and reconnect to missing streams every minute
 
-#Install service file to run websocket server from start of Pi
-
-#Edit file permissions to allow scripts to run:
-chmod 744 ~/hik/*.sh
-chmod 744 ~/hik/*.py
+#start running server
+echo "Start server"
+~/hik/hik-server-running.sh
 
 #End of install.
 echo "Installation complete"
