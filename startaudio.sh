7f2e3abdc7f227a786c0f0f15ed61aea3c43209f
obrien-james <jameswobrien1987@gmail.com>
2022-01-13 13:54:00 +0000

    Update startaudio.sh

--- startaudio.sh
+++ startaudio.sh
@@ -9,8 +9,8 @@
 
 ## Get stream window size from passed parameter
 ## Passed parameters: dispay, window, base, camera number, stream name. 
-## $1=Display; $2=Window; $3=Base; $4=Camera; $5=Name for SCREEN
+## $1=Display; $2=Base; $3=Camera; $4=Name for SCreen
 
 echo "Starting Audio Stream"
 echo "          Camera - ""$4" 
-screen -dmS "$5" sh -c 'omxplayer --avdict rtsp_transport:tcp --win "'"0 0 1 1"'" --layer 0 --vol 10 --live --threshold 0 --timeout 30 --display '"$1"' '"$3""$4"
+screen -dmS "$4" sh -c 'omxplayer --avdict rtsp_transport:tcp --win "'"0 0 1 1"'" --layer 0 --vol 10 --live --threshold 0 --timeout 30 --display '"$1"' '"$2""$3"
